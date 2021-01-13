52e7072186c60561f13b307c09cbcebe0aaf589c
michaelost <mykhailo@bstock.com>
2021-01-13 18:26:00 +0200

    commit

---
+++ vim/selectTag.sh
@@ -0,0 +1,16 @@
+xdotool sleep 0.3 key slash
+xdotool type '<'
+xdotool key Return
+xdotool type 'w'
+
+xdotool type 'vawy'
+xdotool type 'ma'
+
+xdotool keydown Shift + key 8
+xdotool keyup Shift 
+
+
+xdotool type 'V'
+
+xdotool type '`a'
+xdotool type '0'
